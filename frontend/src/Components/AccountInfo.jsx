ea02775ac8d6ca51e24624863e026e8935240160
Jadline <njerijadline@gmail.com>
2025-11-07 00:22:41 +0300

    made modifications

--- frontend/src/Components/AccountInfo.jsx
+++ frontend/src/Components/AccountInfo.jsx
@@ -267,15 +267,7 @@ export default function AccountInfo() {
             </div>
           </div>
 
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold text-indigo-400 hover:text-indigo-300"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
+         
         </div>
       </div>
     </>
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -237,15 +237,6 @@ export default function Login() {
             </div>
           </div>
 
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold text-indigo-400 hover:text-indigo-300"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
     </>
